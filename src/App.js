8f4664795b322661444f5f4cad8ba23ca45036ee
Miruna <elenaned177@gmail.com>
2021-07-14 15:54:52 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const things = ["Go for a run", "Make the dinner", "Listening to music", "d", "e"];
+  const [name, setName] = useState("initialState")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {name}</h1>
+      <button onClick={() => (setName("Martin"))}>Say hi to Martin</button>
+      <div>
+        {things.map(todo => <p>{todo}</p>)}
+      </div>
+
     </div>
   );
 }
