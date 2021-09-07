772454b66818c069b9b82ffef0b49ef616b3e464
Miruna <elenaned177@gmail.com>
2021-09-07 11:10:35 +0200

    useEffect done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Todo from './Todo'
 
-const initialState = ["clean kitchen", "do the dishes", "programming"]
+//intial value of todos gonna be whatever is in local storage
+
+function getTodoFromLocalStorage() {
+
+  let todoString = localStorage.getItem('todos')
+  if (todoString.length > 0) {
+    return todoString.split(',')
+  } else {
+    return []
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState(initialState)
+  const [todos, setTodos] = useState(getTodoFromLocalStorage())
   const [inputValue, setInputValue] = useState("")
+
+  function removeTodo(todo) {
+    setTodos(todos.filter((td) => (td !== todo)))
+  }
+  // use effect will be called 
+  //when the second argument will get updated(in this case, todos)
+  //so setTodos will be called and after that useEffect
+  useEffect(() => {
+    localStorage.setItem('todos', todos)
+  }, [todos])
   return (
     <div>
       <p>Todo List </p>
@@ -20,7 +40,7 @@ function App() {
         //clean up the field
         setInputValue("")
       }}>Add Todo</button>
-      {todos.map(todo => <Todo todo={todo}></Todo>)}
+      {todos.map(todo => <Todo todo={todo} removeTodo={removeTodo}></Todo>)}
     </div>
   )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,13 @@
 import React from "react"
 
 
-export default function Todo(props) {
+export default function Todo({ todo, removeTodo }) {
     return (
         <div className="todos">
-            {props.todo}
+            <p>{todo}</p>
+            <button className="remove" onClick={(e) => {
+                removeTodo(todo)
+            }}>X</button>
         </div>
     )
 }
